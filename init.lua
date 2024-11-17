dca14a74690d3749e718ff9a3d5cebeb80ff0d1d
nikola-s6 <nikola.stojilkovic6@gmail.com>
2024-11-17 02:24:33 +0100

    delete uneccessary code

--- init.lua
+++ init.lua
@@ -219,16 +219,6 @@ require("lazy").setup({
   --     require("onedark").load()
   --   end,
   -- },
-  -- {
-  --   "tiagovla/tokyodark.nvim",
-  --   opts = {
-  --     -- custom options here
-  --   },
-  --   config = function(_, opts)
-  --     require("tokyodark").setup(opts) -- calling setup is optional
-  --     vim.cmd([[colorscheme tokyodark]])
-  --   end,
-  -- },
 
   {
     "projekt0n/github-nvim-theme",
