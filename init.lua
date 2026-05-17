5cef7d9affc9ae1f9a2834644200beeb5ea34ce5
nikola-s6 <nikola.stojilkovic6@gmail.com>
2026-05-17 23:38:20 +0200

    change cmp and improve some minor things

--- init.lua
+++ init.lua
@@ -387,8 +387,8 @@ require("lazy").setup({
 			-- Useful status updates for LSP.
 			{ "j-hui/fidget.nvim", opts = {} },
 
-			-- Allows extra capabilities provided by nvim-cmp
-			"hrsh7th/cmp-nvim-lsp",
+			-- Allows extra capabilities provided by blink.cmp
+			"saghen/blink.cmp",
 		},
 		config = function()
 			--  This function gets run when an LSP attaches to a particular buffer.
@@ -557,7 +557,7 @@ require("lazy").setup({
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -609,6 +609,8 @@ require("lazy").setup({
 				templ = {
 					filetypes = { "templ" },
 				},
+
+				tailwindcss = {},
 			}
 
 			-- Ensure the servers and tools above are installed
@@ -702,122 +704,48 @@ require("lazy").setup({
 	},
 
 	{ -- Autocompletion
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
+		"saghen/blink.cmp",
+		event = "VimEnter",
+		version = "1.*",
 		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
 			{
 				"L3MON4D3/LuaSnip",
+				version = "2.*",
 				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
 					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 						return
 					end
 					return "make install_jsregexp"
 				end)(),
-				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+				opts = {},
+			},
+			"folke/lazydev.nvim",
+		},
+		--- @module 'blink.cmp'
+		--- @type blink.cmp.Config
+		opts = {
+			keymap = {
+				-- 'enter' preset = default keymaps + <CR> to accept.
+				-- Preserves <C-n>/<C-p> nav, <C-Space> trigger, <C-b>/<C-f> doc scroll.
+				preset = "enter",
+				-- Keep your existing snippet jump bindings instead of <Tab>/<S-Tab>.
+				["<C-l>"] = { "snippet_forward", "fallback" },
+				["<C-h>"] = { "snippet_backward", "fallback" },
+			},
+			appearance = { nerd_font_variant = "mono" },
+			completion = {
+				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "lazydev" },
+				providers = {
+					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 				},
 			},
-			"saadparwaiz1/cmp_luasnip",
-
-			-- Adds other completion capabilities.
-			--  nvim-cmp does not ship with all sources by default. They are split
-			--  into multiple repos for maintenance purposes.
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
+			snippets = { preset = "luasnip" },
+			fuzzy = { implementation = "lua" },
+			signature = { enabled = true },
 		},
-		config = function()
-			-- See `:help cmp`
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
-
-				-- For an understanding of why these mappings were
-				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
-				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-
-					-- Scroll the documentation window [b]ack / [f]orward
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
-					-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
-					--['<CR>'] = cmp.mapping.confirm { select = true },
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
-					["<C-Space>"] = cmp.mapping.complete({}),
-
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-				}),
-				sources = {
-					{
-						name = "lazydev",
-						-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-						group_index = 0,
-					},
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-					{ name = "nvim_lsp_signature_help" },
-				},
-			})
-		end,
 	},
 
 	{ -- You can easily change to a different colorscheme.
@@ -896,15 +824,26 @@ require("lazy").setup({
 			ensure_installed = {
 				"bash",
 				"c",
+				"css",
 				"diff",
+				"go",
 				"html",
+				"javascript",
+				"json",
+				"jsonc",
 				"lua",
 				"luadoc",
 				"markdown",
 				"markdown_inline",
 				"query",
+				"rust",
+				"scss",
+				"templ",
+				"tsx",
+				"typescript",
 				"vim",
 				"vimdoc",
+				"yaml",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
@@ -1102,7 +1041,7 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function()
 	harpoon:list():add()
 end, { desc = "Harpoon add to list" })
-vim.keymap.set("n", "<leader>d", function()
+vim.keymap.set("n", "<leader>hx", function()
 	harpoon:list():remove()
 end, { desc = "Harpoon remove from list" })
 vim.keymap.set("n", "<leader>p", function()
@@ -1179,3 +1118,19 @@ vim.api.nvim_create_user_command("Qfd", function(opts)
 	vim.fn.setqflist({}, "r", { items = new_qf })
 	print(string.format("Removed quickfix lines: %s", args))
 end, { nargs = "?", desc = "Delete quickfix entries by their list position" })
+
+-- Kill the prettierd daemon so it re-reads .prettierrc on next save.
+-- Run this after editing a project's .prettierrc if formatting still uses old settings.
+vim.api.nvim_create_user_command("PrettierdRestart", function()
+	vim.fn.jobstart({ "pkill", "-f", "prettierd" }, {
+		on_exit = function(_, code)
+			vim.schedule(function()
+				if code == 0 or code == 1 then
+					vim.notify("prettierd killed — next save will reload .prettierrc", vim.log.levels.INFO)
+				else
+					vim.notify("pkill failed with exit code " .. code, vim.log.levels.ERROR)
+				end
+			end)
+		end,
+	})
+end, { desc = "Kill prettierd daemon to force .prettierrc reload" })
